a2fb5f45dd91fb162971ac1cd335dd0116e49362
DenSad81 <tagil81@mail.ru>
2024-06-19 17:34:41 +0300

    correct some problems

--- Program.cs
+++ Program.cs
@@ -10,40 +10,39 @@ class Program
     {
         const int OffsetForShow = 1;
         int[,] array = new int[3, 6];
+        int arrayRows = array.GetLength(0);
+        int arrayColumns = array.GetLength(1);
         int minValueInArray = 0;
-        int maxValueInArray = 9;
-        int summDigitInRow = 0;
+        int maxValueInArray = 10;
+        int rowSumm = 0;
         int rowForSumm = 1;
-        int productDigitInColumn = 1;
+        int columnProduct = 1;
         int columnForProduct = 0;
         Random random = new Random();
 
-        int arrayQuantityOfRow = array.GetLength(0);
-        int arrayQuantityOfColumn = array.GetLength(1);
-
-        for (int i = 0; i < arrayQuantityOfRow; i++)
+        for (int i = 0; i < arrayRows; i++)
         {
-            for (int j = 0; j < arrayQuantityOfColumn; j++)
+            for (int j = 0; j < arrayColumns; j++)
                 array[i, j] = random.Next(minValueInArray, maxValueInArray);
         }
 
-        for (int i = 0; i < arrayQuantityOfRow; i++)
+        for (int i = 0; i < arrayRows; i++)
         {
-            for (int j = 0; j < arrayQuantityOfColumn; j++)
+            for (int j = 0; j < arrayColumns; j++)
                 Console.Write(array[i, j]);
 
             Console.WriteLine();
         }
 
-        for (int j = 0; j < arrayQuantityOfColumn; j++)
-            summDigitInRow += array[rowForSumm, j];
+        for (int j = 0; j < arrayColumns; j++)
+            rowSumm += array[rowForSumm, j];
 
         Console.WriteLine();
-        Console.WriteLine("Сумма чисел строки № " + (rowForSumm + OffsetForShow) + " = " + summDigitInRow);
+        Console.WriteLine("Сумма чисел строки № " + (rowForSumm + OffsetForShow) + " = " + rowSumm);
 
-        for (int i = 0; i < arrayQuantityOfRow; i++)
-            productDigitInColumn *= array[i, columnForProduct];
+        for (int i = 0; i < arrayRows; i++)
+            columnProduct *= array[i, columnForProduct];
 
-        Console.WriteLine("Произведение чисел колонки № " + (columnForProduct + OffsetForShow) + " = " + productDigitInColumn);
+        Console.WriteLine("Произведение чисел колонки № " + (columnForProduct + OffsetForShow) + " = " + columnProduct);
     }
 }
